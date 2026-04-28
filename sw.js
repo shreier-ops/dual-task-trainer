0cf4923d1d88adf8e799d67d79b6b7244437c651
shreier-ops <shreier@gmail.com>
2026-04-28 20:35:41 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'dualtask-v1';
+const FILES = ['/dual-task-trainer/', '/dual-task-trainer/index.html', '/dual-task-trainer/manifest.json', '/dual-task-trainer/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
